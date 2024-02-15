9f210a72c55b9cdad8809da328ff05d5c68f529e
Raja Upadhyay <upraja96@outlook.com>
2024-02-15 22:20:57 +0000

    wip

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,39 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import string
 
+def int_to_string(x: int) -> str:        
+    res = []
+    sign = ""
+    if x < 0:
+        sign = "-"
+        x = abs(x)
 
-def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    while True:
+        res.append(str(x%10))
+        x //= 10
+        if not x:
+            break
+
+    res = res[::-1]
+    res = sign + "".join(res)
+    return res
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    sign = +1
+    if s[0] == "+":
+        s = s[1:]
+    if s[0] == "-":
+        sign = -1
+        s = s[1:]
+
+    res = 0
+
+    for idx, int_char in enumerate(s[::-1]):
+        res += (10**idx)*string.digits.index(int_char)
+
+    return res * sign
 
 
 def wrapper(x, s):
