c50d1d09daa8c1d5f8608dd1790baa663592b3d6
Raja Upadhyay <upraja96@outlook.com>
2024-02-21 19:57:41 +0000

    wip

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -8,10 +8,55 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
+# def lca(node0: BinaryTreeNode,
+#         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+#     nodes_seen = set([node0])
+
+#     while node0:
+#         node0 = node0.parent
+#         nodes_seen.add(node0)
+    
+#     while node1:
+#         if node1 in nodes_seen:
+#             return node1
+#         node1 = node1.parent
+
+#     return None
+
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    len_0 = 0
+    len_1 = 0
+
+    node0_tmp = node0
+    node1_tmp = node1
+
+    while node0_tmp:
+        len_0 += 1
+        node0_tmp = node0_tmp.parent
+
+    while node1_tmp:
+        len_1 += 1
+        node1_tmp = node1_tmp.parent
+
+    if len_0 > len_1:
+        diff = len_0 - len_1
+
+        while diff > 0:
+            node0 = node0.parent
+            diff -= 1
+
+    if len_1 > len_0:
+        diff = len_1 - len_0
+        while diff > 0:
+            node1 = node1.parent
+            diff -= 1
+
+    while node0 and node1 and node0 != node1:
+        node1 = node1.parent
+        node0 = node0.parent
+
+    return node0
 
 
 @enable_executor_hook
